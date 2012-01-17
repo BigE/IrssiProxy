71cab9890bb34d32654ad3acd9442904d8be48f0
Eric Gach <eric@php-oop.net>
2012-01-17 10:44:47 -0600

    Inital commit

---
+++ README.md
@@ -0,0 +1,15 @@
+IrssiProxy
+==========
+
+IrssiProxy is a simple Python application that uses pygtk to connect to an
+irssi client using the irc proxy module. It will then monitor all incoming
+messages and searh the text for specified strings. If a string is found, it
+will then pop up a notification box to tell the user what channel, user and
+what was said.
+
+Prerequisits
+------------
+python
+pygtk
+pynotify
+
---
+++ src/irssiproxy.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pynotify
+import os.path
+import socket
+import threading
+
+__author__="Eric Gach <eric@php-oop.net>"
+__date__ ="$Jan 13, 2012 12:47:17 PM$"
+__version__ = "0.1-dev"
+
+class IrssiProxy(gtk.Window):
+	def __init__(self):
+		super(IrssiProxy, self).__init__(gtk.WINDOW_TOPLEVEL)
+		pynotify.init("IrssiProxy")
+		self.set_title("IrssiProxy Configuration")
+		self.set_default_size(320, 240)
+		self._icon = os.path.realpath(os.path.dirname(__file__)+"/resources/65704.png")
+		self._initStatusIcon()
+		self._initLayout()
+		self._irc = None
+		self.connect("destroy", self.destroy)
+		self.set_icon_from_file(self._icon)
+		self.set_position(gtk.WIN_POS_CENTER_ALWAYS)
+		self.show()
+
+	def destroy(self, widget, data=None):
+		if self._irc != None:
+			self._irc.stop()
+		gtk.main_quit()
+
+	def on_connect(self, widget, data=None):
+		self._irc = IrssiProxyConnection(self, self._host.get_text(), self._port.get_text(), self._password.get_text())
+		self._irc.start()
+
+	def on_disconnect(self, widget, data=None):
+		self._irc.stop()
+		self._irc = None
+
+	def popup_statusIcon_menu(self, status_icon, button, activate_time, *args):
+		menu = gtk.Menu()
+		quit = gtk.MenuItem("Quit")
+		about = gtk.MenuItem("About")
+		about.connect("activate", self.show_about)
+		quit.connect("activate", self.destroy)
+		menu.append(about)
+		menu.append(quit)
+		menu.show_all()
+		menu.popup(None, None, gtk.status_icon_position_menu, button, activate_time, status_icon)
+
+	def push_msg(self, message):
+		self._statusBar.pop(self._context)
+		self._statusBar.push(self._context, message)
+
+	def show_about(self, widget, data=None):
+		about_dialog = gtk.AboutDialog()
+
+		about_dialog.set_destroy_with_parent(True)
+		about_dialog.set_name("IrssiProxy Notification")
+		about_dialog.set_version(__version__)
+		about_dialog.set_authors([__author__])
+
+		about_dialog.run()
+		about_dialog.destroy()
+
+	def _initLayout(self):
+		mainVBox = gtk.VBox(False, 20)
+		serverHBox = gtk.HBox(False, 20)
+		serverHBox.pack_start(gtk.Label("Irssi Proxy Server"), False, False, 10)
+		self._host = gtk.Entry()
+		serverHBox.pack_end(self._host, False, False, 10)
+		mainVBox.pack_start(serverHBox, False, False, 10)
+		portHBox = gtk.HBox(False, 20)
+		portHBox.pack_start(gtk.Label("Irssi Proxy Port"), False, False, 10)
+		self._port = gtk.Entry()
+		self._port.set_width_chars(8)
+		self._port.set_text("6667")
+		portHBox.pack_end(self._port, False, False, 10)
+		mainVBox.pack_start(portHBox, False, False, 10)
+		passHBox = gtk.HBox(False, 20)
+		passHBox.pack_start(gtk.Label("Irssi Proxy Password"), False, False, 10)
+		self._password = gtk.Entry()
+		self._password.set_visibility(False)
+		passHBox.pack_end(self._password, False, False, 10)
+		mainVBox.pack_start(passHBox, False, False, 10)
+		self._statusBar = gtk.Statusbar()
+		self._context = self._statusBar.get_context_id("General Settings")
+		self._statusBar.push(self._context, "Disconnected...")
+		mainVBox.pack_end(self._statusBar, False, False, 10)
+		buttonHBox = gtk.HBox(False, 20)
+		self.btnDisconnect = gtk.Button("Disconnect", gtk.STOCK_DISCONNECT)
+		self.btnDisconnect.set_sensitive(False)
+		self.btnDisconnect.connect("clicked", self.on_disconnect)
+		buttonHBox.pack_end(self.btnDisconnect, False)
+		self.btnConnect = gtk.Button("Connect", gtk.STOCK_CONNECT)
+		self.btnConnect.connect("clicked", self.on_connect)
+		buttonHBox.pack_end(self.btnConnect, False)
+		mainVBox.pack_end(buttonHBox, False, False, 10)
+		mainVBox.show_all()
+		self.add(mainVBox)
+
+	def _initStatusIcon(self):
+		statusIcon = gtk.status_icon_new_from_file(self._icon)
+		statusIcon.set_title("IrssiProxy")
+		statusIcon.connect("popup-menu", self.popup_statusIcon_menu)
+		statusIcon.set_tooltip('IrssiProxy Notifications')
+
+class IrssiProxyConnection(threading.Thread):
+	def __init__(self, irssi, host, port, password):
+		self.stopthread = threading.Event()
+		self.irssi = irssi
+		self.host = host
+		self.port = port
+		self.password = password
+		threading.Thread.__init__(self)
+
+	def run(self):
+		gtk.threads_enter()
+		self.irssi.btnConnect.set_sensitive(False)
+		self.irssi.push_msg("Connecting to %s ... " % (self.host))
+		gtk.threads_leave()
+		for res in socket.getaddrinfo(self.host, self.port, socket.AF_UNSPEC, socket.SOCK_STREAM):
+			e = None
+			af,socktype,proto,canoname,sa = res
+			try:
+				self.sock = socket.socket(af, socktype, proto)
+			except socket.error, e:
+				self.sock = None
+				continue
+			try:
+				self.sock.connect(sa)
+			except socket.error, e:
+				self.sock = None
+				continue
+		if e:
+			gtk.threads_enter()
+			self.irssi.btnConnect.set_sensitive(True)
+			gtk.threads_leave()
+			raise e
+
+		gtk.threads_enter()
+		self.irssi.btnDisconnect.set_sensitive(True)
+		self.irssi.push_msg("Connected to %s" % (self.host))
+		gtk.threads_leave()
+		self.sock.setblocking(0)
+		self.send("PASS %s" % (self.password))
+		self.send("NICK IrssiProxy")
+		self.send("USER IrssiProxy 0 * :IrssiProxy")
+		while not self.stopthread.isSet():
+			try:
+				data = self.recv()
+				print "Recv: %s" % (data.strip())
+			except socket.error, e:
+				if e.errno != 11:
+					self.stop()
+					print e
+		self.sock = None
+		gtk.threads_enter()
+		self.irssi.btnDisconnect.set_sensitive(False)
+		self.irssi.btnConnect.set_sensitive(True)
+		self.irssi.push_msg("Disconnected...")
+		gtk.threads_leave()
+
+	def recv(self):
+		return self.sock.recv(2048)
+
+	def send(self, buffer):
+		print "Sent: " + buffer
+		return self.sock.sendall(buffer + "\r\n")
+
+	def stop(self):
+		self.stopthread.set()
+
+if __name__ == "__main__":
+	gtk.gdk.threads_init()
+	IrssiProxy()
+	gtk.gdk.threads_enter()
+	gtk.main()
+	gtk.gdk.threads_leave()
