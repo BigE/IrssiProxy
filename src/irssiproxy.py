4adc35f7427e19524cf7c0b9eeb3d5de39578e8c
Eric Gach <eric@php-oop.net>
2012-01-21 00:48:13 -0600

    Updated to actually work off a user supplied regex. Notifications now pop up and display for 10 seconds until closed automatically.

--- src/irssiproxy.py
+++ src/irssiproxy.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# vim: set fileencoding=utf8 :
 
 import pygtk
 pygtk.require('2.0')
 import gtk
 import pynotify
 import os.path
+import re
 import socket
 import threading
 
@@ -24,10 +24,16 @@ class IrssiProxy(gtk.Window):
 		self._initStatusIcon()
 		self._initLayout()
 		self._irc = None
+		self.connect("delete-event", self.delete)
 		self.connect("destroy", self.destroy)
 		self.set_icon_from_file(self._icon)
 		self.set_position(gtk.WIN_POS_CENTER_ALWAYS)
 		self.show()
+
+	def delete(self, widget, data=None):
+		self.hide()
+		# Stop the window from being deleted
+		return True
 
 	def destroy(self, widget, data=None):
 		if self._irc != None:
@@ -57,6 +63,9 @@ class IrssiProxy(gtk.Window):
 		self._statusBar.pop(self._context)
 		self._statusBar.push(self._context, message)
 
+	def show(self, icon = None):
+		super(IrssiProxy, self).show()
+
 	def show_about(self, widget, data=None):
 		about_dialog = gtk.AboutDialog()
 
@@ -68,13 +77,26 @@ class IrssiProxy(gtk.Window):
 		about_dialog.run()
 		about_dialog.destroy()
 
+	def _checkRegex(self, entry):
+		regex = entry.get_text()
+		print "checking: %s" % (regex)
+		try:
+			re.compile(regex)
+			entry.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse('black'))
+		except re.error, e:
+			print e
+			entry.modify_text(gtk.STATE_NORMAL, gtk.gdk.color_parse('red'))
+
 	def _initLayout(self):
+		# Main Box
 		mainVBox = gtk.VBox(False, 20)
+		# Server Box
 		serverHBox = gtk.HBox(False, 20)
 		serverHBox.pack_start(gtk.Label("Irssi Proxy Server"), False, False, 10)
 		self._host = gtk.Entry()
 		serverHBox.pack_end(self._host, False, False, 10)
 		mainVBox.pack_start(serverHBox, False, False, 10)
+		# Port Box
 		portHBox = gtk.HBox(False, 20)
 		portHBox.pack_start(gtk.Label("Irssi Proxy Port"), False, False, 10)
 		self._port = gtk.Entry()
@@ -82,16 +104,26 @@ class IrssiProxy(gtk.Window):
 		self._port.set_text("6667")
 		portHBox.pack_end(self._port, False, False, 10)
 		mainVBox.pack_start(portHBox, False, False, 10)
+		# Password Box
 		passHBox = gtk.HBox(False, 20)
 		passHBox.pack_start(gtk.Label("Irssi Proxy Password"), False, False, 10)
 		self._password = gtk.Entry()
 		self._password.set_visibility(False)
 		passHBox.pack_end(self._password, False, False, 10)
 		mainVBox.pack_start(passHBox, False, False, 10)
+		# Match Box
+		matchHBox = gtk.HBox(False, 20)
+		matchHBox.pack_start(gtk.Label("Regex to Match"), False, False, 10)
+		self.match = gtk.Entry()
+		self.match.connect("changed", self._checkRegex)
+		matchHBox.pack_end(self.match, False, False, 10)
+		mainVBox.pack_start(matchHBox, False, False, 10)
+		# Status bar
 		self._statusBar = gtk.Statusbar()
 		self._context = self._statusBar.get_context_id("General Settings")
 		self._statusBar.push(self._context, "Disconnected...")
 		mainVBox.pack_end(self._statusBar, False, False, 10)
+		# Button Box
 		buttonHBox = gtk.HBox(False, 20)
 		self.btnDisconnect = gtk.Button("Disconnect", gtk.STOCK_DISCONNECT)
 		self.btnDisconnect.set_sensitive(False)
@@ -101,6 +133,7 @@ class IrssiProxy(gtk.Window):
 		self.btnConnect.connect("clicked", self.on_connect)
 		buttonHBox.pack_end(self.btnConnect, False)
 		mainVBox.pack_end(buttonHBox, False, False, 10)
+		# show everything
 		mainVBox.show_all()
 		self.add(mainVBox)
 
@@ -108,6 +141,7 @@ class IrssiProxy(gtk.Window):
 		statusIcon = gtk.status_icon_new_from_file(self._icon)
 		statusIcon.set_title("IrssiProxy")
 		statusIcon.connect("popup-menu", self.popup_statusIcon_menu)
+		statusIcon.connect("activate", self.show)
 		statusIcon.set_tooltip('IrssiProxy Notifications')
 
 class IrssiProxyConnection(threading.Thread):
@@ -155,6 +189,13 @@ class IrssiProxyConnection(threading.Thread):
 			try:
 				data = self.recv()
 				print "Recv: %s" % (data.strip())
+				m = re.match(":([^\s]+)![^\s]+\sPRIVMSG\s([^\s]+)\s:(.*)", data)
+				if m is not None and re.search(self.irssi.match.get_text(), m.group(3)):
+					n = pynotify.Notification("%s on %s → %s" % (m.group(1), m.group(2), m.group(3)))
+					n.set_urgency(pynotify.URGENCY_NORMAL)
+					n.set_timeout(10)
+					n.connect("closed", lambda e : n.close())
+					n.show()
 			except socket.error, e:
 				if e.errno != 11:
 					self.stop()
